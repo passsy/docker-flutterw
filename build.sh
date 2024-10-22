b43717e15e671ad6903347a8090334292fbc8c1a
Pascal Welsch <pascal@phntm.xyz>
2024-10-22 19:40:44 +0200

    Never use arm64, since flutter does not fully support it

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@ set -ex
 docker pull ubuntu:focal
 
 cd flutterw-base
-docker build -t passsy/flutterw:base-latest .
+docker build --platform linux/amd64 -t passsy/flutterw:base-latest .
 cd ..
 
 cd flutterw-android
-docker build -t passsy/flutterw:android-latest .
+docker build --platform linux/amd64 -t passsy/flutterw:android-latest .
 cd ..
